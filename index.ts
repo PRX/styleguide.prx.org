d79fff29a131cf4c92cd486e6adb64775ce3d6a4
sandikbarr <sandi.barr@prx.org>
2017-04-13 18:29:19 -0500

    doesn't actually need named exports here

--- index.ts
+++ index.ts
@@ -1,9 +1,2 @@
 export * from './src/app/components';
-export { ChartsModule,
-  IndexedChartModel, LegendItemModel, TimeseriesChartModel, TimeseriesDatumModel,
-// TODO: read somewhere that components should also be exported for component libs, need to confirm
-  LegendComponent, LegendItemComponent,
-  LineIndexedChartComponent, PieChartComponent,
-  StackedBarCategoryRotatedChartComponent,
-  StackedBarTimeseriesChartComponent,
-  LineTimeseriesChartComponent } from 'chart.prx.org';
+export * from 'chart.prx.org';
