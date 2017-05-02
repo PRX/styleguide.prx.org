4f2afc58d9c7f99f45afbe43a0c8c1d331c8ee4d
cavis <ryancavis@gmail.com>
2017-05-02 16:43:23 -0600

    Timepicker demo

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'demo-app',
   template: `
-    <h1>Date Picker</h1>
+    <h1>PRX Styleguide Demo</h1>
     <section>
+      <h2>Date Picker</h2>
       <ul>
         <li>The prx-datepicker is an ng2 wrapper for <a href="https://github.com/dbushell/Pikaday">Pikaday</a></li>
         <li>
@@ -19,15 +20,30 @@ import { Component } from '@angular/core';
         </li>
       </ul>
       <prx-datepicker [date]="today" (dateChange)="onDateChange($event)"></prx-datepicker>
+      <span class="changed" *ngIf="dateChanged">Changed: {{dateChanged}}</span>
+    </section>
+    <section>
+      <h2>Time Picker</h2>
+      <ul>
+        <li>Something something blah blah blah.</li>
+      </ul>
+      <prx-timepicker [date]="today" (timeChange)="onTimeChange($event)"></prx-timepicker>
+      <span class="changed" *ngIf="timeChanged">Changed: {{timeChanged}}</span>
     </section>
   `,
 })
 export class AppComponent {
 
   today = new Date();
+  dateChanged: string;
+  timeChanged: string;
 
   onDateChange(value: Date) {
-    console.log(value + 'is an instanceof Date === ' + (value instanceof Date));
+    this.dateChanged = value.toString();
+  }
+
+  onTimeChange(value: Date) {
+    this.timeChanged = value.toString();
   }
 
 }
