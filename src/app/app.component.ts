a4db2d5c35b235226247f40b7caf096eb439a744
sandikbarr <sandi.barr@prx.org>
2017-05-31 13:53:04 -0500

    add placeholder in the demo app to show for navuser on smaller screen sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,9 @@ import { Component } from '@angular/core';
   template: `
     <prx-header>
       <prx-navitem route="/" text="PRX StyleGuide"></prx-navitem>
-      <prx-navuser userName="Mary"></prx-navuser> 
+      <prx-navuser userName="Mary">
+        <div class="user-loaded profile-image-placeholder"></div>
+      </prx-navuser> 
     </prx-header>
     <main>
       <article>
@@ -13,5 +15,25 @@ import { Component } from '@angular/core';
       </article>
     </main>
   `,
+  styles: [`
+    /* this placeholder should go away once we have the ImageLoaderComponent moved over and use it in this example */
+    .profile-image-placeholder {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      border: 1px solid rgb(112, 142, 151);
+      background-color: #7b9fa7;
+      display: inline-block;
+      vertical-align: middle;
+      margin-left: 10px;
+    }
+    @media screen and (min-width: 768px) {
+      .profile-image-placeholder {
+        border-width: 2px;
+        width: 40px;
+        height: 40px;
+       }
+     }
+  `]
 })
 export class AppComponent { }
