c866f96b25cf3fd6e0932055a9606c35d9d44ecf
cavis <ryancavis@gmail.com>
2017-06-02 10:16:13 -0600

    TTL handling for root docs

--- src/lib/src/hal/hal.service.ts
+++ src/lib/src/hal/hal.service.ts
@@ -13,26 +13,26 @@ export class HalService {
 
   constructor(private http: Http, @Optional() private auth: AuthService) {}
 
-  public(host: string, path = '/api/v1'): HalObservable<HalDoc> {
-    return this.remoteGet(this.loadRemote(host, false), path);
+  public(host: string, path = '/api/v1', ttl?: number): HalObservable<HalDoc> {
+    return this.remoteGet(this.loadRemote(host, false, ttl), path);
   }
 
-  authorized(host: string, path = '/api/v1'): HalObservable<HalDoc> {
+  authorized(host: string, path = '/api/v1', ttl?: number): HalObservable<HalDoc> {
     if (!this.auth) {
       throw new Error('Tried to load authorized HAL resource without <prx-auth>!');
     }
-    return this.remoteGet(this.loadRemote(host, true), path);
+    return this.remoteGet(this.loadRemote(host, true, ttl), path);
   }
 
-  private remoteGet(remote: HalRemote, path): HalObservable<HalDoc> {
+  private remoteGet(remote: HalRemote, path: string): HalObservable<HalDoc> {
     let link = HalLink.from(path);
     return <HalObservable<HalDoc>> remote.get(link).map(obj => new HalDoc(obj, remote));
   }
 
-  private loadRemote(host: string, withAuth: boolean): HalRemote {
+  private loadRemote(host: string, withAuth: boolean, ttl: number): HalRemote {
     let key = withAuth ? `${host}.auth` : `${host}.public`;
     if (!this.remotes[key]) {
-      this.remotes[key] = new HalRemote(host, this.http, withAuth ? this.auth : null, 0);
+      this.remotes[key] = new HalRemote(host, this.http, withAuth ? this.auth : null, ttl);
     }
     return this.remotes[key];
   }
--- src/lib/src/hal/remote/halremote.ts
+++ src/lib/src/hal/remote/halremote.ts
@@ -23,6 +23,7 @@ export class HalHttpError extends Error {
 export class HalRemote {
 
   cache: HalCache;
+  rootTTL = 300;
 
   constructor(
     private host: string,
@@ -30,10 +31,10 @@ export class HalRemote {
     private auth?: AuthService,
     private ttl?: number
   ) {
+    this.cache = new HalCache(this.host.replace(/\./g, '-'), ttl);
     if (!host.match(/^http/)) {
       this.host = host.match(/\.org|\.tech/) ? `https://${host}` : `http://${host}`;
     }
-    this.cache = new HalCache(this.host.replace('.', '-'), ttl);
   }
 
   switchHost(link?: HalLink): HalRemote {
@@ -60,7 +61,9 @@ export class HalRemote {
 
   get(link: HalLink, params: {} = null): Observable<{}> {
     let href = this.expand(link, params);
-    if (href) {
+    if (href && this.isRoot(href)) {
+      return this.cache.cache(href, this.httpRequest('get', href), this.rootTTL);
+    } else if (href) {
       return this.cache.cache(href, this.httpRequest('get', href));
     } else {
       return Observable.throw(new HalLinkError('No link object specified!'));
@@ -88,46 +91,60 @@ export class HalRemote {
   }
 
   private httpRequest(method: string, href: string, body?: string, allowRetry = true): Observable<Response> {
-    return this.httpOptions(body ? true : false)
-      .mergeMap(opts => {
-        if (method === 'put' || method === 'post') {
-          return this.http[method](href, body, opts);
-        } else {
-          return this.http[method](href, opts);
-        }
-      })
-      .catch(res => Observable.of(res)) // don't throw http errors
-      .mergeMap(res => {
-        if ((method === 'get' || method === 'put') && res.status === 200) {
-          return Observable.of(res.json());
-        } else if (method === 'put' && res.status === 204) {
-          return Observable.of(null);
-        } else if (method === 'post' && res.status === 201) {
-          return Observable.of(res.json());
-        } else if (method === 'delete' && res.status === 204) {
-          return Observable.of(null);
-        } else if (res.status === 401 && allowRetry && this.auth) {
-          return this.auth.refreshToken().mergeMap(() => this.httpRequest(method, href, body, false));
-        } else {
-          return Observable.throw(new HalHttpError(res.status, `Got ${res.status} from ${method.toUpperCase()} ${href}`));
-        }
-      });
+    return this.getResponse(method, href, body).mergeMap(res => {
+      if ((method === 'get' || method === 'put') && res.status === 200) {
+        return Observable.of(res.json());
+      } else if (method === 'put' && res.status === 204) {
+        return Observable.of(null);
+      } else if (method === 'post' && res.status === 201) {
+        return Observable.of(res.json());
+      } else if (method === 'delete' && res.status === 204) {
+        return Observable.of(null);
+      } else if (res.status === 401 && allowRetry && this.auth) {
+        return this.auth.refreshToken().mergeMap(() => this.httpRequest(method, href, body, false));
+      } else if (res.status === 0) {
+        return Observable.throw(new Error(`CORS preflight failed for ${method.toUpperCase()} ${href}`));
+      } else {
+        return Observable.throw(new HalHttpError(res.status, `Got ${res.status} from ${method.toUpperCase()} ${href}`));
+      }
+    });
   }
 
-  private httpOptions(hasContent = false): Observable<RequestOptionsArgs> {
+  private getResponse(method: string, href: string, body?: string): Observable<Response> {
     let headers = new Headers();
     headers.append('Accept', 'application/hal+json');
-    if (hasContent) {
+    if (body) {
       headers.append('Content-Type', 'application/hal+json');
     }
-    if (this.auth) {
-      return this.auth.token.first().map(tokenString => {
+
+    // wait for auth token - but not for root api paths!
+    let options: Observable<RequestOptionsArgs> = Observable.of({headers: headers});
+    if (this.auth && !this.isRoot(href)) {
+      options = this.auth.token.first().map(tokenString => {
         headers.append('Authorization', `Bearer ${tokenString}`);
         return {headers: headers};
       });
-    } else {
-      return Observable.of({headers: headers});
     }
+
+    // make request, and catch http errors
+    return options.mergeMap(opts => {
+      if (method === 'put' || method === 'post') {
+        return this.http[method](href, body, opts);
+      } else {
+        return this.http[method](href, opts);
+      }
+    }).catch(err => {
+      if (err instanceof Response) {
+        return Observable.of(err);
+      } else {
+        throw err;
+      }
+    });
+  }
+
+  private isRoot(href: string): boolean {
+    let path = href.replace(/^http(s)?:\/\/[^\/]+\/?/, '');
+    return path === '' || path === 'api/v1' || path === 'api/v1/';
   }
 
 }
