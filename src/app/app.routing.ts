0da415a7b06a000891d741911c3a0abe1f6aa05a
Sandi Barr <sandi.barr@prx.org>
2017-06-08 18:46:09 -0500

    document reset.css

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,13 +6,21 @@ import { AuthDemoComponent } from './auth/auth-demo.component';
 import { ChartsDemoComponent } from './charts/charts-demo.component';
 import { DatepickerDemoComponent } from './datepicker/datepicker-demo.component';
 import { HalDemoComponent } from './hal/hal-demo.component';
+import { HeaderDemoComponent } from './nav/header-demo.component';
+import { NavItemDemoComponent } from './nav/navitem-demo.component';
+import { NavUserDemoComponent } from './nav/navuser-demo.component';
+import { ResetDemoComponent } from './global-css/reset-demo.component';
 
 export const routes: Routes = [
   { path: '',           component: TocComponent },
   { path: 'auth',       component: AuthDemoComponent },
   { path: 'charts',     component: ChartsDemoComponent },
   { path: 'datepicker', component: DatepickerDemoComponent },
-  { path: 'hal',        component: HalDemoComponent }
+  { path: 'hal',        component: HalDemoComponent },
+  { path: 'header',        component: HeaderDemoComponent },
+  { path: 'navitem',        component: NavItemDemoComponent },
+  { path: 'navuser',        component: NavUserDemoComponent },
+  { path: 'reset',      component: ResetDemoComponent }
 ];
 
 export const routingComponents: any[] = [
@@ -20,7 +28,11 @@ export const routingComponents: any[] = [
   AuthDemoComponent,
   ChartsDemoComponent,
   DatepickerDemoComponent,
-  HalDemoComponent
+  HalDemoComponent,
+  HeaderDemoComponent,
+  NavItemDemoComponent,
+  NavUserDemoComponent,
+  ResetDemoComponent
 ];
 
 export const routingProviders: any[] = [];
