1aa0acbd9d5b03a75cc5c5ab14af98310454ad21
Sandi Barr <sandi.barr@prx.org>
2017-06-22 16:31:05 -0500

    lol jk auth token won't work unless you run chrome without web security and still doesn't get IndividualAccount for some reason

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,23 @@ import { AuthService, HalService, HalDoc } from 'ngx-prx-styleguide';
 @Component({
   selector: 'demo-app',
   template: `
-    <prx-auth *ngIf="loadAuth" [host]="authHost" [client]="authClient">
+    <prx-auth [host]="authHost" [client]="authClient">
     </prx-auth>
     <prx-header>
       <prx-navitem route="/" text="PRX StyleGuide"></prx-navitem>
-      <prx-navuser [userName]="userName">
-        <div class="user-loading profile-image-placeholder"></div>
-        <prx-image *ngIf="userImageDoc" class="user-loaded" [imageDoc]="userImageDoc"></prx-image>
-      </prx-navuser> 
+      <prx-navuser userName="Mary">
+        <div class="user-loaded profile-image-placeholder"></div>
+      </prx-navuser>
     </prx-header>
     <main>
       <article>
-        <section class="main demo">
+        <section class="main">
           <router-outlet></router-outlet>
         </section>
       </article>
     </main>
   `,
   styles: [`
-    /* this placeholder should go away once we have the ImageLoaderComponent moved over and use it in this example */
     .profile-image-placeholder {
       width: 30px;
       height: 30px;
@@ -42,30 +40,4 @@ import { AuthService, HalService, HalDoc } from 'ngx-prx-styleguide';
      }
   `]
 })
-export class AppComponent {
-  authHost = 'id-staging.prx.tech';
-  authClient = 'lVN05vLI8aCADh7lzbrL0AkDvEfPNuoEPpL2umL5';
-  cmsHost = 'cms-staging.prx.tech';
-  userImageDoc: HalDoc;
-  userName: string;
-
-  constructor(
-    private auth: AuthService,
-    private hal: HalService,
-  ) {
-    auth.token.subscribe(token => this.loadAccount(token));
-  }
-
-  loadAccount(token: string) {
-    if (token) {
-      this.hal.authorized(this.cmsHost).followItems('prx:accounts').subscribe((docs: HalDoc[]) => {
-        const individualAccount = docs.find(d => d['type'] === 'IndividualAccount');
-        this.userImageDoc = individualAccount;
-        this.userName = individualAccount['name'];
-      });
-    } else {
-      this.userImageDoc = null;
-      this.userName = null;
-    }
-  }
-}
+export class AppComponent { }
