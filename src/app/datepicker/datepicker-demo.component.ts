0b7e48d5bf129b72a8d7b8e43152a710d20d8976
Sandi Barr <sandi.barr@prx.org>
2017-09-22 12:06:11 -0500

    datepicker reflects changes to @Input() date

--- src/app/datepicker/datepicker-demo.component.ts
+++ src/app/datepicker/datepicker-demo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'datepicker-demo',
@@ -7,21 +8,40 @@ import { Component } from '@angular/core';
       <h1>Datepicker Module</h1>
       <section>
         <h2>Date Picker</h2>
+        <p>The prx-datepicker is an ng2 wrapper for <a href="https://github.com/dbushell/Pikaday">Pikaday</a></p>
+        <dl>
+          <dt>selector</dt><dd><code>prx-datepicker</code></dd>
+        </dl>
         <ul>
-          <li>The prx-datepicker is an ng2 wrapper for <a href="https://github.com/dbushell/Pikaday">Pikaday</a></li>
           <li>
-            It takes an optional Input <code>date</code> of type <code>Date</code> and an optional
-            Output that has a parameter of type <code>Date</code>
+            <code>@Input() date: Date</code> (optional) bound input to sets datepicker date
           </li>
           <li>
-            If a date has been selected or entered by the user, the datepicker's input control will have the class <code>changed</code>.
+           <code>@Output() dateChange: EventEmitter&lt;Date&gt;</code> (optional) emitted when date is selected
+          </li>
+          <li>
+            <code>@Input() changed: boolean</code> (optional) if true, applies the class changed to the <code>input</code> element
           </li>
           <li>
             If the user enters an invalid date, the datepicker's input control will have the class <code>invalid</code>.
           </li>
         </ul>
-        <prx-datepicker [date]="today" (dateChange)="onDateChange($event)"></prx-datepicker>
-        <span class="changed" *ngIf="dateChanged">Changed: {{dateChanged}}</span>
+        <aside>
+          Usage:
+          <pre class="code">
+            &lt;prx-datepicker [date]="today" (dateChange)="onDateChange($event)"&gt;&lt;/prx-datepicker&gt;
+          </pre>
+          Example:
+          <prx-datepicker [date]="today" (dateChange)="onDateChange($event)"></prx-datepicker>
+          <span class="changed" *ngIf="dateChanged">Changed: {{dateChanged}}</span>
+          <hr>
+          <div>
+            <p>Demonstrating changing bound <code>@Input() date</code></p>
+            <button (click)="timeTravel(-1, 'weeks')">Go Back One Week</button>
+            <button (click)="timeTravel(-1, 'months')">Go Back One Month</button>
+            <button (click)="timeTravel(-1, 'years')">Go Back One Year</button>
+          </div>
+        </aside>
       </section>
       <section>
         <h2>Time Picker</h2>
@@ -42,6 +62,7 @@ export class DatepickerDemoComponent {
   timeChanged: string;
 
   onDateChange(value: Date) {
+    this.today = value;
     this.dateChanged = value.toString();
   }
 
@@ -49,4 +70,7 @@ export class DatepickerDemoComponent {
     this.timeChanged = value.toString();
   }
 
+  timeTravel(howMuch: number, period: string) {
+    this.today = moment(this.today).add(howMuch, period).toDate();
+  }
 }
