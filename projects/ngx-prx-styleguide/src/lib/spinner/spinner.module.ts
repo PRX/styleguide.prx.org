e54cfde1b9ce0a77d716854de969b976d713df87
Sandi Barr <sandi.barr@prx.org>
2017-10-12 17:10:33 -0500

    adds overlay spinner component and service

--- projects/ngx-prx-styleguide/src/lib/spinner/spinner.module.ts
+++ projects/ngx-prx-styleguide/src/lib/spinner/spinner.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { OverlaySpinnerComponent } from './overlay.spinner.component';
+import { OverlaySpinnerService } from './overlay.spinner.service';
 import { SpinnerComponent } from './spinner.component';
 
 @NgModule({
   declarations: [
+    OverlaySpinnerComponent,
     SpinnerComponent
   ],
   exports: [
+    OverlaySpinnerComponent,
     SpinnerComponent
   ],
   imports: [
     CommonModule
   ],
-  providers: []
+  providers: [
+    OverlaySpinnerService
+  ]
 })
 
 export class SpinnerModule { }
