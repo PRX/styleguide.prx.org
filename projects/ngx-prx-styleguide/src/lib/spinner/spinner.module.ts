d02987b94ac4496c8322a3b67ea2fc1273d116ca
Sandi Barr <sandi.barr@prx.org>
2017-10-13 10:36:51 -0500

    absolute and fixed position overlay and loading message options on prx-spinner

--- projects/ngx-prx-styleguide/src/lib/spinner/spinner.module.ts
+++ projects/ngx-prx-styleguide/src/lib/spinner/spinner.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { OverlaySpinnerComponent } from './overlay.spinner.component';
-import { OverlaySpinnerService } from './overlay.spinner.service';
 import { SpinnerComponent } from './spinner.component';
 
 @NgModule({
   declarations: [
-    OverlaySpinnerComponent,
     SpinnerComponent
   ],
   exports: [
-    OverlaySpinnerComponent,
     SpinnerComponent
   ],
   imports: [
     CommonModule
   ],
-  providers: [
-    OverlaySpinnerService
-  ]
+  providers: []
 })
 
 export class SpinnerModule { }
