6784bf3d27ab6a37fe954ada5d1d9c405ab279c5
Peter Karman <peter.karman@prx.org>
2018-04-03 14:45:18 -0500

    Add getUserDoc() method relying on HAL and userinfo.href

--- src/lib/src/auth/userinfo.service.ts
+++ src/lib/src/auth/userinfo.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Headers, Http, RequestOptionsArgs, RequestOptions } from '@angular/http';
+import { HalDoc } from '../hal/doc/haldoc';
+import { HalObservable } from '../hal/doc/halobservable';
+import { HalService } from '../hal/hal.service';
 
 export class Userinfo {
   sub: number;
@@ -9,14 +12,16 @@ export class Userinfo {
   family_name: string;
   preferred_username: string;
   apps: any;
+  href: string;
 }
 
 @Injectable()
 export class UserinfoService {
 
   authHost: string;
+  userinfo: Userinfo;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private hal: HalService) { }
 
   config(authHost: string) {
     this.authHost = authHost;
@@ -29,6 +34,11 @@ export class UserinfoService {
     return this.http.get(url, options)
                     .map(response => response.json() as Userinfo)
                     .catch(this.handleError);
+  }
+
+  getUserDoc(userinfo: Userinfo): HalObservable<HalDoc> {
+    let url = userinfo.href.match(/^(https?:\/\/.+?)(\/.+)/);
+    return this.hal.public(url[1], url[2]);
   }
 
   private handleError(error: any): Promise<any> {
