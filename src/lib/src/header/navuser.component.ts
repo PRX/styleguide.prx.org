ecbea93bcf87c15108ae17b2da5946b82186473f
Peter Karman <peter.karman@prx.org>
2018-04-04 15:46:26 -0500

    Add userinfo apps to navuser menu

--- src/lib/src/header/navuser.component.ts
+++ src/lib/src/header/navuser.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Userinfo } from '../auth/userinfo.service';
 
 @Component({
   moduleId: module.id,
@@ -8,14 +9,22 @@ import { Component, Input } from '@angular/core';
     './navuser.component.css'
   ],
   template: `
-    <div class="nav-holder">
-      <ng-template [ngIf]="userName">
-        <a *ngIf="userName">
-          <span class="name">{{userName}}</span>
+    <div class="nav-holder nav-userinfo">
+      <ng-template [ngIf]="userinfo">
+        <a *ngIf="userinfo">
+          <span class="name">{{userinfo.preferred_username}}</span>
           <ng-content select=".user-loaded"></ng-content>
         </a>
+        <div *ngIf="userinfo" class="nav-userinfo-menu-apps">
+          <ul class="nav-userinfo-apps">
+            <li><a class="nav-userinfo-app" href="https://exchange.prx.org/">PRX Exchange</a></li>
+            <li *ngFor="let appName of appNames();">
+              <a class="nav-userinfo-app" href="{{userinfo.apps[appName]}}">{{appLabel(appName)}}</a>
+            </li>
+          </ul>
+        </div>
       </ng-template>
-      <div *ngIf="!userName" class="spin-holder">
+      <div *ngIf="!userinfo" class="spin-holder">
         <ng-content select=".user-loading"></ng-content>
       </div>
     </div>
@@ -23,5 +32,18 @@ import { Component, Input } from '@angular/core';
 })
 
 export class NavUserComponent {
-  @Input() userName: string;
+  @Input() userinfo: Userinfo;
+
+  appLabel(appName: string): string {
+    let n = appName.replace(/^https?:\/\//, '').replace(/\..+/, '');
+    return 'PRX ' + n.charAt(0).toUpperCase() + n.slice(1);
+  }
+
+  appNames(): any[] {
+    let names = new Array();
+    for (let appName in this.userinfo.apps) {
+      names.push(appName);
+    }
+    return names;
+  }
 }
