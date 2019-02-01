dace76d015fe88e8f332833452305265e7b8b67b
Peter St.Jean <peter@stjean.co>
2019-02-01 16:02:35 -0600

    Generating library using CLI

---
+++ projects/ngx-prx-styleguide/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-prx-styleguide
+ */
+
+export * from './lib/ngx-prx-styleguide.service';
+export * from './lib/ngx-prx-styleguide.component';
+export * from './lib/ngx-prx-styleguide.module';
