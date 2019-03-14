3359e3649d3315769a4aef73cdb19598c19ae07d
Rick Peterman <rpeterman@pri.org>
2019-03-14 14:44:53 -0400

    Add timepicker stories

--- projects/ngx-prx-styleguide/src/lib/datepicker/timepicker.component.stories.ts
+++ projects/ngx-prx-styleguide/src/lib/datepicker/timepicker.component.stories.ts
@@ -3,24 +3,6 @@ import { action } from '@storybook/addon-actions';
 import { centered } from '@storybook/addon-centered/angular';
 import { withKnobs, date, boolean, text } from '@storybook/addon-knobs';
 import { DatepickerModule } from './datepicker.module';
-import { Component, ViewChild, ElementRef } from '@angular/core';
-
-// Need a component defined to demo container prop usage.
-@Component ({
-  selector:'open-picker',
-  template: `
-    <div class="centered-wrapper">
-      <prx-datepicker [container]="calendar"></prx-datepicker>
-      <div #calendar></div>
-    </div>
-  `,
-  styles: [
-    ':host >>> .pika-single.container { border: 1px solid #e6e6e6; border-top: 0; }'
-  ]
-})
-class OpenPickerComponent {
-  @ViewChild('calendar') calendar:ElementRef;
-}
 
 // Module metadata for stories.
 const storiesModuleMetaData = moduleMetadata({
@@ -32,7 +14,7 @@ const storiesModuleMetaData = moduleMetadata({
   providers: [],
 });
 
-storiesOf('Forms Controls|Inputs/Date Picker', module)
+storiesOf('Forms Controls|Inputs/Time Picker', module)
   .addDecorator(withKnobs)
   .addDecorator(centered)
   .addDecorator(storiesModuleMetaData)
@@ -40,7 +22,6 @@ storiesOf('Forms Controls|Inputs/Date Picker', module)
     'Usage Details (Knobs)',
     () => {
       const changed = boolean('Appear Changed', false);
-      const dateFormat = text('Date Format', 'MM/DD/YYYY');
       const useUTC = boolean('Use UTC', false);
       const today = new Date();
       // Default day can't change, so we need to construct the day, not the second.
@@ -52,27 +33,25 @@ storiesOf('Forms Controls|Inputs/Date Picker', module)
         return useUTC ? new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds())) : d;
       };
       const dataDate = dateKnob('Initial Date', defaultDate);
-      const onDateChange = (newDate: Date) => {
+      const onTimeChange = (newDate: Date) => {
         action('Date Changed')({date: newDate, string: useUTC ? newDate.toUTCString() : newDate.toString()});
       };
 
       return {
         template: `
           <div class="centered-wrapper">
-            <prx-datepicker
-              [format]="dateFormat"
+            <prx-timepicker
               [date]="dataDate"
-              (dateChange)="onDateChange($event)"
+              (timeChange)="onTimeChange($event)"
               [UTC]="useUTC"
               [changed]="changed"
-            ></prx-datepicker>
+            ></prx-timepicker>
           </div>
         `,
         props: {
           changed,
-          dateFormat,
           dataDate,
-          onDateChange,
+          onTimeChange,
           useUTC
         }
       }
@@ -80,57 +59,62 @@ storiesOf('Forms Controls|Inputs/Date Picker', module)
     {
       notes: {
         markdown:`
-# Date Picker
+# Time Picker
 
-The prx-datepicker is an ng2 wrapper for [Pikaday](https://github.com/dbushell/Pikaday).
+Input to set time portion of dates at 30 minute increments. Use in combination with Date Picker.
 
 ----
 
 __Module__ \`DatepickerModule\`
 
-__Selector__ \`prx-datepicker\`
+__Selector__ \`prx-timepicker\`
 
 ----
 
-- \`@Input() format: string = 'MM/DD/YYYY'\` \\- _(optional)_ Format shown in the form field. Formatting options are [moment](https://momentjs.com/docs/#/displaying/format/) based.
-- \`@Input() container: ElementRef\` \\- _(optional)_ Element reference for an always open calendar picker.
 - \`@Input() date: Date | string\` \\- _(optional)_ Sets initial date of the picker.
 - \`@Input() changed: boolean = false\` \\- _(optional)_ If true, applies the class \`changed\` to the input element.
 - \`@Input() UTC: boolean = false\` \\- _(optional)_ Have picker use and return UTC dates.
-- \`@Output() dateChange: EventEmitter<Date>\` \\- _(optional)_ Emitted when date is selected.
+- \`@Output() timeChange: EventEmitter<Date>\` \\- _(optional)_ Emitted when time is selected.
 `
       }
     }
   );
 
-storiesOf('Forms Controls|Inputs/Date Picker/Examples', module)
+storiesOf('Forms Controls|Inputs/Time Picker/Examples', module)
   .addDecorator(centered)
   .addDecorator(storiesModuleMetaData)
   .add(
-    'Always Open Picker',
-    () => ({
-      component: OpenPickerComponent
-    }),
+    'With Date Picker',
+    () => {
+      const dataDate = new Date();
+      const onDateChange = action('Date Changed');
+
+      return {
+        template: `
+        <div class="centered-wrapper">
+          <prx-datepicker
+            [date]="dataDate"
+            (dateChange)="onDateChange($event)"
+          ></prx-datepicker>
+          <prx-timepicker
+            [date]="dataDate"
+            (timeChange)="onDateChange($event)"
+          ></prx-timepicker>
+        </div>
+        `,
+        props: {
+          dataDate,
+          onDateChange
+        }
+      };
+    },
     {
       notes: {
         markdown: `
 ### Usage
-\`\`\`javascript
-@Component ({
-  selector:'open-picker',
-  template: \`
-    <div class="centered-wrapper">
-      <prx-datepicker [container]="calendar"></prx-datepicker>
-      <div #calendar></div>
-    </div>
-  \`,
-  styles: [
-    ':host >>> .pika-single.container { border: 1px solid #e6e6e6; border-top: 0; }'
-  ]
-})
-class OpenPickerComponent {
-  @ViewChild('calendar') calendar:ElementRef;
-}
+\`\`\`html
+<prx-datepicker [date]="dataDate" (dateChange)="onDateChange($event)" ></prx-datepicker>
+<prx-timepicker [date]="dataDate" (timeChange)="onDateChange($event)" ></prx-timepicker>
 \`\`\`
         `
       }
