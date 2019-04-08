2edf25b22f7b478d2616d92438c9a253dbb70e7d
Peter St.Jean <peter@stjean.co>
2019-04-08 17:12:45 -0500

    Fiddly localstorage mock

--- projects/ngx-prx-styleguide/src/lib/model/base.storage.spec.ts
+++ projects/ngx-prx-styleguide/src/lib/model/base.storage.spec.ts
@@ -1,8 +1,16 @@
 import { BaseStorage } from './base.storage';
 
 describe('BaseStorage', () => {
+  let jsdomLocalstorageSetItem;
 
-  beforeEach(() => BaseStorage.clear());
+  beforeEach(() => {
+    jsdomLocalstorageSetItem = Storage.prototype.setItem
+    BaseStorage.clear()
+  });
+
+  afterEach(() => {
+    Storage.prototype.setItem = jsdomLocalstorageSetItem 
+  })
 
   it('uses localstorage by default', () => {
     expect(localStorage.length).toEqual(0);
@@ -18,7 +26,7 @@ describe('BaseStorage', () => {
   });
 
   it('falls back to in-memory storage', () => {
-    jest.spyOn(localStorage, 'setItem').mockImplementation(() => { throw new Error('no localstorage')});
+    jest.spyOn(Storage.prototype, 'setItem').mockImplementation(() => { throw new Error('no localstorage')});
     BaseStorage.setItem('foo', {id: 'foo'});
     expect(localStorage.length).toEqual(0);
     expect(BaseStorage.getItem('foo')).toEqual({id: 'foo'});
@@ -29,8 +37,8 @@ describe('BaseStorage', () => {
   });
 
   it('falls back at any time', () => {
-    let originalSetItem = localStorage.setItem.bind(localStorage);
-    jest.spyOn(localStorage, 'setItem').mockImplementation((key: string, val: any) => {
+    let originalSetItem = Storage.prototype.setItem.bind(localStorage);
+    jest.spyOn(Storage.prototype, 'setItem').mockImplementation((key: string, val: any) => {
       if (key === 'foo') {
         originalSetItem('foo', val);
       } else {
