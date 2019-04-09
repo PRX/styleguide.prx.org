22e0cec2569d686daa5fb118f5f6d098b67def40
Rick Peterman <rpeterman@globalpost.com>
2019-04-09 14:09:47 -0400

    Remove sticky positioning from status bar

--- projects/ngx-prx-styleguide/src/lib/status-bar/status-bar.component.stories.ts
+++ projects/ngx-prx-styleguide/src/lib/status-bar/status-bar.component.stories.ts
@@ -5,6 +5,7 @@ import { ImageModule } from '../image/image.module';
 import { ModuleWithProviders } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { centered } from '@storybook/addon-centered/angular';
 
 const routing: ModuleWithProviders = RouterModule.forRoot([]);
 
@@ -24,13 +25,13 @@ const storiesModuleMetaData = moduleMetadata({
 
 
 storiesOf('Navigation|Status Bar', module)
+  .addDecorator(centered)
   .addDecorator(storiesModuleMetaData)
   .add(
     'Usage Details',
     () => ({
       template: `
       <div class="main">
-        <div class="header"></div>
         <prx-status-bar>
           <a prx-status-bar-link routerLink="/some/place">
             <prx-icon name="chevron-left"></prx-icon>
@@ -48,18 +49,7 @@ storiesOf('Navigation|Status Bar', module)
           --header-height: 50px;
         }
         .main {
-          position: relative;
-          height: 200vh;
-          padding-top: calc(var(--header-height) + 130px);
-          border-bottom: 30vh solid #1a1a1a;
-        }
-        .header {
-          position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          height: var(--header-height);
-          background-color: #1a1a1a;
+          width: 90vw;
         }
         `
       ]
