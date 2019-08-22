f5cea2865da070a1c964fe1bf978addd792222eb
Sandi Barr <sandi.barr@prx.org>
2019-08-22 11:36:43 -0500

    throwing errors, old, deleted

--- src/app/geo/index.ts
+++
@@ -1,7 +0,0 @@
-import { GeoChartMapDemoComponent } from './geochart-map-demo.component';
-import { GeoChartTableDemoComponent } from './geochart-table-demo.component';
-
-export const geoDemoComponents: any[] = [
-  GeoChartMapDemoComponent,
-  GeoChartTableDemoComponent
-];
