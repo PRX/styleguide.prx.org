97b0882cb7e111b8b6121c75e2730004d31a115e
cavis <ryancavis@gmail.com>
2021-06-25 10:36:20 -0600

    Restrict total audio size (#229)
    * Just linting
    
    * Make sure total file size stays under 250mb
    
    * Add exported vars
    
    * Don't lie about MB vs MiB

--- projects/ngx-prx-styleguide/src/lib/upload/model/invalid/audio.invalid.ts
+++ projects/ngx-prx-styleguide/src/lib/upload/model/invalid/audio.invalid.ts
@@ -59,6 +59,12 @@ export const VERSION_TEMPLATED = (template?: HalDoc): BaseInvalid => {
       if (nonMatches.length) {
         return `Non-matching audio files (${nonMatches.join(' / ')})`;
       }
+
+      // max total file size (250mb)
+      const totalSize = undeleted.reduce((acc, f) => acc + (f.size || 0), 0);
+      if (totalSize > (250 * 1024 * 1024)) {
+        return 'Total audio file size must be less than 250 MiB';
+      }
     }
 
     return null;
